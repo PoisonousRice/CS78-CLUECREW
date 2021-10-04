7dbe1083e48e1ad67ce69d901c9474e5ae1fbee1
Thomas Schuff <tschuff5814@student.pps.net>
2021-10-04 13:30:21 -0700

    scripts

--- CS78-CLUECREW/Assets/Scripts/PlayerControl.cs
+++ CS78-CLUECREW/Assets/Scripts/PlayerControl.cs
@@ -7,29 +7,93 @@ public class PlayerControl : MonoBehaviour
 {
     public string playerName;
     public NavMeshAgent agent;
-    int energy = 0;
-    bool myTurn;
-
+    int energy;
+    public bool isMyTurn = true;
+    bool moving = false;
     
+    [HideInInspector] 
+    public int horizontal;
+    public int vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        myTurn();
     }
 
+    void myTurn()
+    {
+        energy = rollDie();
+        Debug.Log(energy);
+        //Popup text with # of energy
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        if (myTurn) {
-            energy = rollDie();
-            //Popup text with # of energy
+        if(isMyTurn && !moving)
+        {
+            if (energy > 0)
+            {
+                moving = true;
+                horizontal = (int)Input.GetAxisRaw("Horizontal");
+                vertical = (int)Input.GetAxisRaw("Vertical");
+                if ((horizontal != 0) | (vertical != 0))
+                {
+                    move(horizontal, vertical);
+                }
+                else
+                {
+                    moving = false;
+                }
+            }
         }
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
     }
 
-    int rollDie() {
-        int temp = Random.Range(1, 6) + Random.Range(1, 6);
-        return temp;
+    int rollDie () 
+    {
+        return Random.Range(1, 6);
+    }
+
+    void move (float horizontal, float vertical) 
+    {
+        if (horizontal == -1)
+        {
+            Debug.Log("Left");
+            //move left
+            agent.SetDestination(new Vector3(transform.position.x, transform.position.y, transform.position.z + 1));
+            energy -= 1;
+            Debug.Log(energy);
+            moving = false;
+        }
+        else if (horizontal == 1)
+        {
+            Debug.Log("Right");
+            //move right
+            agent.SetDestination(new Vector3(transform.position.x, transform.position.y, transform.position.z - 1));
+            energy -= 1;
+            Debug.Log(energy);
+            moving = false;
+        }
+        else if (vertical == -1)
+        {
+            Debug.Log("Down");
+            //move down
+            agent.SetDestination(new Vector3(transform.position.x - 1, transform.position.y, transform.position.z));
+            energy -= 1;
+            Debug.Log(energy);
+            moving = false;
+        }
+        else if (vertical == 1)
+        {
+            Debug.Log("Up");
+            //move up
+            agent.SetDestination(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z));
+            energy -= 1;
+            Debug.Log(energy);
+            moving = false;
+        }
+        vertical = 0;
+        horizontal = 0;
     }
 }
