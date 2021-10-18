622512ece715f27f86c7911b7d67e0e1459698cd
Thomas Schuff <tschuff5814@student.pps.net>
2021-10-18 13:35:28 -0700

    FRINGERS CROSSED

--- CS78-CLUECREW/Assets/Scripts/AIControl.cs
+++ CS78-CLUECREW/Assets/Scripts/AIControl.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class AIControl : MonoBehaviour
 {
+    public GameObject[] players = new GameObject[6]; // Mr. Green, Colonel Mustard, Ms. Peacock, Professor Plum, Scarlett, Ms. White
+    public MouseScript mouse;
+    GameObject activePlayer;
+    PlayerControl apScript;
+    public int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activePlayer = players[0];
+        mouse.player = activePlayer;
+        apScript = activePlayer.GetComponent<PlayerControl>();
+        apScript.myTurn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(apScript.energy == 0)
+        {
+            if (count == 5){count = 0;}
+            else {count += 1;}
+            activePlayer = players[count];
+            mouse.player = activePlayer;
+            apScript = activePlayer.GetComponent<PlayerControl>();
+            apScript.myTurn();
+        }
     }
 }
---
+++ CS78-CLUECREW/Assets/Scripts/MouseScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseScript : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public Camera cam;
+    public GameObject player;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseClick();
+        }
+    }
+
+    void mouseClick()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hitData, Mathf.Infinity, layerMask))
+        {
+            GameObject hitObject = hitData.transform.gameObject;
+            PlayerControl tempScript = player.GetComponent<PlayerControl>();
+            tempScript.checkSquare(hitObject);
+        }
+    }
+}
