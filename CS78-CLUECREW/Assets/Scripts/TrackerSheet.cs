63fd6a280f97379d3058a1f9404507254f766017
Kate <krodgers8686@student.pps.net>
2021-10-25 13:48:39 -0700

    hall floors and tracker sheet

--- CS78-CLUECREW/Assets/Scripts/TrackerSheet.cs
+++ CS78-CLUECREW/Assets/Scripts/TrackerSheet.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class TrackerSheet : MonoBehaviour
 {
-    
 
+    public LayerMask layerMask;
+    public Camera cam;
+    public Material Material1;
 
-    void Start()
-    {
-        
-    }
-
-   
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseClick();
+        }
     }
+
+    void mouseClick()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitData, Mathf.Infinity, layerMask))
+        {
+            GameObject hitObject = hitData.transform.gameObject;
+            hitObject.GetComponent<MeshRenderer>().material = Material1;
+        }
+    }
+
 }
